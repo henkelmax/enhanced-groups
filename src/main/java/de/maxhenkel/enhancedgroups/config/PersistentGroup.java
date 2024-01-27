af55bfb9d915c8173470efae7f8de74f43e15842
Max Henkel <max@maxhenkel.de>
2024-01-27 13:54:09 +0100

    Add hidden group option to persistent group command

--- src/main/java/de/maxhenkel/enhancedgroups/config/PersistentGroup.java
+++ src/main/java/de/maxhenkel/enhancedgroups/config/PersistentGroup.java
@@ -12,18 +12,20 @@ public class PersistentGroup {
     @Nullable
     private final String password;
     private final Type type;
+    private final boolean hidden;
     @Nullable
     private UUID id;
 
-    public PersistentGroup(String name, @Nullable String password, Type type, @Nullable UUID id) {
+    public PersistentGroup(String name, @Nullable String password, Type type, boolean hidden, @Nullable UUID id) {
         this.name = name;
         this.password = password;
         this.type = type;
+        this.hidden = hidden;
         this.id = Objects.requireNonNullElseGet(id, UUID::randomUUID);
     }
 
-    public PersistentGroup(String name, @Nullable String password, Type type) {
-        this(name, password, type, null);
+    public PersistentGroup(String name, @Nullable String password, Type type, boolean hidden) {
+        this(name, password, type, hidden, null);
     }
 
     public String getName() {
@@ -37,6 +39,10 @@ public class PersistentGroup {
 
     public Type getType() {
         return type;
+    }
+
+    public boolean isHidden() {
+        return hidden;
     }
 
     public UUID getId() {
